4f5cae3643c9d799122fe46a98503ccdc189c33f
gavin <gallmond@gmail.com>
2018-06-29 21:10:01 +0100

    add pm2 conf

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'gallmond',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-35-177-127-109.eu-west-2.compute.amazonaws.com',
+      key: '~/.ssh/id_rsa.pub',
+      ref: 'origin/master',
+      repo: 'git@github.com:Gallmond/gallmondnet.git',
+      path: '/home/ec2-user/projects/gallmondnet',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
+/home/ec2-user/projects
