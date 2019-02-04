e5a0f5098b3ef627b62a6c449a4ae23400cf797f
gallmond <gallmond+gitkracken@gmail.com>
2019-02-04 14:57:17 +0000

    mongo testing

--- fwf_class.js
+++ fwf_class.js
@@ -6,22 +6,39 @@ class foodWithFriends{
         this.mongoClient = require('mongodb').MongoClient;
         this.dbClient;
 
-        // create dbClient
-        this.mongoClient.connect(process.env.GALLMONDNET_MONGODB_URL, { useNewUrlParser: true }, (err, client)=>{
-            if(err){
-                    console.log("err could not connect to mongodb", err);
-            }
-            this.dbClient = client;
-        });
-
         return this;
+
+    }
+
+    async db(){
+        var result = await this.dbPromise();
+        return result;          
+    }
+
+    dbPromise(){
+        return new Promise((_pobj)=>{
+            if(this.dbClient){
+                return _pobj({"success":true, "dbClient":this.dbClient});
+            }
+            // create dbClient
+            this.mongoClient.connect(process.env.GALLMONDNET_MONGODB_URL, { useNewUrlParser: true }, (err, client)=>{
+                if(err){
+                        console.log("err could not connect to mongodb", err);
+                        return _pobj({"error":"err could not connect to mongodb", "info": err});
+                }
+                this.dbClient = client;
+                return _pobj({"success":true, "dbClient":client});
+            });
+        });
     }
 
     clientExists(){
         if(this.dbClient){
             console.log("client was made", this.dbClient);
+            console.log("client was made");
         }else{
             console.log("was not made", this.dbClient);
+            console.log("was not made");
         }
     }
 
